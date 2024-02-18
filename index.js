c100b47cf584c6041b2c7b8216be3a28b7361ae2
Mtalhaa832 <mtalhaa832@gmail.com>
2024-02-18 11:07:21 +0500

    'kk'

---
+++ index.js
@@ -0,0 +1,19 @@
+// task 1
+// function booHoo(){
+//     const userName =prompt("enter your name");
+//     alert("wellcome:" +  userName)
+// };
+// booHoo();
+// function color(){
+// const colo = Math.ceil(Math.random());
+// const colorNmae=["red", 'green','yellow','blue']
+// document.write(colo = colorNmae )
+// };
+// color();
+
+
+//task 3
+function numb(){
+const num = Math.ceil(Math.random()*100)
+document.write(num)};
+numb();
